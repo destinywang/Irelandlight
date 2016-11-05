94c88101d3139231dad78c7da8b8e75c2234167d
destiny <destinywk@163.com>
2016-11-06 01:28:39 +0800

    first commit

---
+++ src/main/java/com/irelandlight/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.irelandlight.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by destiny on 2016/11/5.
+ */
+@Controller
+@RequestMapping(value = "/test")
+public class TestController {
+
+    @RequestMapping(value = "/hello")
+    public String hello() {
+        System.out.println("TestController.hello");
+        return "hello";
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/vm/hello.vm
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Hello Worldddddd!</h2>
+</body>
+</html>
