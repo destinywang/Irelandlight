ac7ad4e3dcecf5ac2d7cb8da3f63e433c28bdc3a
destiny <destinywk@163.com>
2016-12-08 13:14:26 +0800

    clear dev

---
+++ src/main/java/com/irelandlight/util/EncryptUtil.java
@@ -0,0 +1,19 @@
+package com.irelandlight.util;
+
+/**
+ * Created by destiny on 2016/12/7.
+ */
+public class EncryptUtil {
+
+    // 全局数组
+    private static final String[] strDigits = { "0", "1", "2", "3", "4", "5",
+            "6", "7", "8", "9", "a", "b", "c", "d", "e", "f" };
+
+    private static final String[] IMAGE_FILE_EXT = {"png", "bmp", "jpg", "jpeg"};
+
+    public static final String IMAGE_DIR = "/Users/destiny/IdeaProjects/toutiao/upload/image/";
+
+    public static final String TOUTIAO_DOMAIN = "http://localhost:8080/";
+
+    public static final String QINIU_DOMAIN = "http://oetw0yrii.bkt.clouddn.com/";
+}
