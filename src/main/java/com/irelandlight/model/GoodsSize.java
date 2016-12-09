df7cea94db9b17ed6dd969d3611e9b1e7646dd34
IncredChen <1041684034@qq.com>
2016-12-09 12:16:34 +0800

    修改order查询

---
+++ src/main/java/com/irelandlight/model/GoodsSize.java
@@ -0,0 +1,11 @@
+package com.irelandlight.model;
+
+/**
+ * Created  with Intellij IDEA.
+ * User: Ele_Chen
+ * Date: 2016/12/8
+ * Time: 9:07
+ * Description: 目的... .
+ */
+public class GoodsSize {
+}
---
+++ src/main/java/com/irelandlight/service/ShopCarGoodsRelationService.java
@@ -0,0 +1,11 @@
+package com.irelandlight.service;
+
+/**
+ * Created  with Intellij IDEA.
+ * User: Ele_Chen
+ * Date: 2016/12/8
+ * Time: 15:18
+ * Description: 目的... .
+ */
+public interface ShopCarGoodsRelationService {
+}
