49770296e9ed70c7404eeb9a408e03d2cfc347a5
Ke.Steve <huangke7296@foxmail.com>
2016-12-09 12:30:15 +0800

    impl goods container func

--- src/main/java/com/irelandlight/dao/CommentMapper.java
+++
@@ -1,7 +0,0 @@
-package com.irelandlight.dao;
-
-/**
- * Created by mac on 2016/12/3.
- */
-public interface CommentMapper {
-}
---
+++ src/main/java/com/irelandlight/model/GoodsSizePrice.java
@@ -0,0 +1,7 @@
+package com.irelandlight.model;
+
+/**
+ * Created by mac on 2016/12/8.
+ */
+public class GoodsSizePrice {
+}
