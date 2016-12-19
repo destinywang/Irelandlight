5629f05c4fcaf8594cb6e5e57b76992a4a054401
zichu <arrleo@163.com>
2016-12-20 05:26:01 +0800

    manager1

---
+++ src/main/java/com/irelandlight/controller/HomePageController.java
@@ -0,0 +1,28 @@
+package com.irelandlight.controller;
+
+import com.irelandlight.service.HomePageService;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import java.util.Map;
+
+/**
+ * Created by Zichu on 2016/12/19.
+ */
+@RequestMapping()
+@ResponseBody
+public class HomePageController{
+    @Resource
+    private HomePageService homePageService;
+    public String homePage(Model model){
+        Map<String,Object> homePageImage=homePageService.queryHomePageImage();
+        Map<String,Object> homePageNews=homePageService.queryNews();
+        Map<String,Object> homePageGoods=homePageService.queryHotGoods();
+        model.addAttribute("homePageImage",homePageImage);
+        model.addAttribute("homePageNews",homePageNews);
+        model.addAttribute("homePageGoods",homePageGoods);
+        return "";
+    }
+}
